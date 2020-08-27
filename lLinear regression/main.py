17a389bd71727b41bf91a4d7e81e6dffca4064a8
can019 <jys01012@gmail.com>
2020-08-27 20:37:28 +0900

    간단한 linear regression

--- lLinear regression/main.py
+++ lLinear regression/main.py
@@ -6,11 +6,12 @@ Y = [1.,3.,5.,7.]  # label set
 
 for step in range(300):  # 300번 훈련을 하겠다.
     hypothesis = W * X  # 가설
-    cost = tf.reduce_mean(tf.square(hypothesis - Y))
+    cost = tf.reduce_mean(tf.square(hypothesis - Y))  # cost 정의
 
-    alpha = 0.01
+    alpha = 0.01  # learning rate
     gradient = tf.reduce_mean(tf.multiply(tf.multiply(W, X)-Y, X))
     descent = W - tf.multiply(alpha, gradient)
     W.assign(descent)
     if step % 10 ==0:
         print('{:5} | {:10.4f} | {:10.6f}'.format(step, cost.numpy(), W.numpy()[0]))
+print('\n Model :: H(x) ={:10.6f}x'.format(W.numpy()[0]))
