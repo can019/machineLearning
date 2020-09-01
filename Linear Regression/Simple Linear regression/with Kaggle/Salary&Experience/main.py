dde9b867de2e62959aa59edf46ea07a53a258230
can019 <jys01012@gmail.com>
2020-09-02 04:46:08 +0900

    max-epoch & learning rate changed

--- Linear Regression/Simple Linear regression/with Kaggle/Salary&Experience/main.py
+++ Linear Regression/Simple Linear regression/with Kaggle/Salary&Experience/main.py
@@ -8,8 +8,8 @@ salary = data['Salary']
 
 w = tf.Variable(2000.0)
 b = tf.Variable(500.0)
-learning_rate = 0.01
-for i in range(100000):
+learning_rate = 0.0001
+for i in range(1000000):
     with tf.GradientTape() as tape:
         hypothesis = w*experience + b
         cost = tf.reduce_mean(tf.square(hypothesis-salary))
@@ -18,7 +18,7 @@ for i in range(100000):
     w.assign_sub(learning_rate * w_grad)
     b.assign_sub(learning_rate * b_grad)
 
-    if i % 1000 == 0:
+    if i % 10000 == 0:
         print("{:5}|{:10.4f}|{:10.4f}|{:10.6f}".format(i, w.numpy(), b.numpy(), cost))
 
 x2 = [1.0, 11.0]
