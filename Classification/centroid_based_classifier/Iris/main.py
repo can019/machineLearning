b904af68ea8b6234489fe25bf1e432ff6f4f7dd9
can019 <jys01012@gmail.com>
2020-09-17 22:26:26 +0900

    fix feature number mistake :: 3->4

--- Classification/centroid_based_classifier/Iris/main.py
+++ Classification/centroid_based_classifier/Iris/main.py
@@ -2,56 +2,51 @@ import numpy as np
 import pandas as pd
 
 read_iris_train_set = pd.read_csv("iris_train.csv", header=None)
-read_iris_test_set = pd.read_csv("iris_test.csv", header=None);
+read_iris_test_set = pd.read_csv("iris_test.csv", header=None)
 
 # data info
-read_iris_train_set.info()
-read_iris_test_set.info()
-
-print(read_iris_test_set)
-print(read_iris_train_set)
+# read_iris_train_set.info()
+# read_iris_test_set.info()
 
 # convert 'DataFrame' to 'numpy'
 dataFrame_irs_train_set = read_iris_train_set[:]
 dataFrame_irs_test_set = read_iris_test_set[:]
-iris_train_set = np.array(dataFrame_irs_train_set, dtype = 'float32');
-iris_test_set = np.array(dataFrame_irs_test_set, dtype = 'float32');
+iris_train_set = np.array(dataFrame_irs_train_set, dtype='float32')
+iris_test_set = np.array(dataFrame_irs_test_set, dtype='float32')
 
-print(iris_train_set.shape)
-print(iris_test_set.shape)
 
-x1, y1, z1, count1 = 0., 0., 0., 0
-x2, y2, z2, count2 = 0., 0., 0., 0
-x3, y3, z3, count3 = 0., 0., 0., 0
+count = np.zeros([3,4])
+meanVector = np.zeros([3,4])
 
-def meanVector(x, y, z, count):
-    result = np.array([x, y, z])
-    result = result/count
+
+for i in range(np.size(iris_train_set,0)):
+
+    if iris_train_set[i][4] == 1:
+        meanVector[0] = meanVector[0] + iris_train_set[i][0:-1]
+        count[0] = count[0]+1
+    elif iris_train_set[i][4] == 2:
+        meanVector[1] = meanVector[1] + iris_train_set[i][0:-1]
+        count[1] = count[1] + 1
+    elif iris_train_set[i][4] == 3:
+        meanVector[2] = meanVector[2] + iris_train_set[i][0:-1]
+        count[2] = count[2] + 1
+
+meanVector = meanVector/count
+distance = np.zeros([3, 1])
+
+def cal_distance_with_meanVector(data,group_num):
+    result = np.sqrt(np.sum(np.square(meanVector[group_num] - data[0:-1])))
     return result
+correct_count = 0
 
-for i in range(120):
-    label = iris_train_set[i, 4]
-    if label == 1:
-        count1 = count1+1
-        x1 = x1 + iris_train_set[i, 0]
-        y1 = y1 + iris_train_set[i, 1]
-        z1 = z1 + iris_train_set[i, 2]
-    elif label == 2:
-        x2 = x2 + iris_train_set[i, 0]
-        y2 = y2 + iris_train_set[i, 1]
-        z2 = z2 + iris_train_set[i, 2]
-        count2 = count2+1
-    else:
-        x3 = x3 + iris_train_set[i, 0]
-        y3 = y3 + iris_train_set[i, 1]
-        z3 = z3 + iris_train_set[i, 2]
-        count3 = count3+1
+for i in range(np.size(iris_test_set,0)):
+    label = iris_test_set[i][-1]
+    distance[0] = cal_distance_with_meanVector(iris_test_set[i], 0)
+    distance[1] = cal_distance_with_meanVector(iris_test_set[i], 1)
+    distance[2] = cal_distance_with_meanVector(iris_test_set[i], 2)
 
-meanVector(x1, y1, z1, count1)
-meanVector(x2, y2, z2, count2)
-meanVector(x3, y3, z3, count3)
+    if np.argmin(distance)+1 == label:
+        correct_count = correct_count+1
 
 
-
-
-
+print("정확도 : {:0.3f}%".format(correct_count/30*100, "."))
