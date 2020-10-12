f1a96a0a05a67466cb6d8891cd2cfeebb462e191
can019 <jys01012@gmail.com>
2020-10-12 22:22:34 +0900

    done

--- Classification/svm_classifier/MNIST_with_non_linear&non_linear/main.py
+++ Classification/svm_classifier/MNIST_with_non_linear&non_linear/main.py
@@ -1,17 +1,20 @@
-from sklearn.datasets import fetch_openml
+from sklearn.model_selection import cross_val_score
 from sklearn.svm import LinearSVC
 from sklearn.svm import SVC
+from sklearn.preprocessing import MinMaxScaler
 import struct
 import os
-import numpy as np
 import pandas as pd
-# line 7 ~ 25 :: ubyte to csv
+import numpy as np
+import time
+
+# line 12 ~ 30 :: ubyte to csv
 if not os.path.isfile("./MNIST.csv"):
     fp_image = open('train-images.idx3-ubyte','rb')
     fp_label = open('train-labels.idx1-ubyte','rb')
     csv_file = open("MNIST.csv",'w',encoding="utf-8")
 
-    mag, lbl_count = struct.unpack(">II", fp_label.read(8))
+    mag, lbl_count = struct.unpack(">II", fp_label.read(8))  # unsigned int *2로
     mag, img_count = struct.unpack(">II", fp_image.read(8))
     rows, cols = struct.unpack(">II", fp_image.read(8))
     pixels = rows*cols
@@ -32,6 +35,43 @@ read_MNIST = pd.read_csv("MNIST.csv")  # pandas로 읽음
 np_MNIST = read_MNIST.to_numpy()  # numpy로 변환
 
 # slice data
-x_data = np_MNIST[:,1:]
-y_data = np_MNIST[:,0]
-print()
+X = np_MNIST[:, 1:]
+Y = np_MNIST[:, 0]
+# print(np.shape(X))  # -> 59999
+
+# scaling
+X_255 = X/255
+X_MinMax = MinMaxScaler().fit(X).transform(X)
+
+# parmeter setting
+debug_Mode = False
+cv_ = 5
+
+# model setting
+linear_model = LinearSVC(max_iter=10000, verbose=debug_Mode)
+non_linear_model = SVC(max_iter=10000, verbose=debug_Mode)
+
+# Run
+print("-----Cross validate :: cv is {} -----".format(cv_))
+
+start1 = time.perf_counter()
+linear_score = cross_val_score(linear_model, X_255, Y, cv=cv_)
+end1 = time.perf_counter()
+print("linear :: Acurracy is {}, {:.2f}sec".format(np.round(linear_score.mean(), 3), end1-start1))
+
+start2 = time.perf_counter()
+non_linear_score = cross_val_score(non_linear_model, X_255, Y, cv=cv_)
+end2 = time.perf_counter()
+print("non_linear :: Acurracy is {}, {:.2f}sec\n".format(np.round(non_linear_score.mean(), 3), end2-start2))
+
+print("-----Cross validate with Min_Max_scaling:: cv is {} -----".format(cv_))
+
+start3 = time.perf_counter()
+linear_score_with_min_max = cross_val_score(linear_model, X_MinMax, Y, cv=cv_)
+end3 = time.perf_counter()
+print("linear :: Acurracy is {}, {:.2f}sec".format(np.round(linear_score_with_min_max.mean(), 3), end3-start3))
+
+start4 = time.perf_counter()
+non_linear_score_with_min_max = cross_val_score(non_linear_model, X_MinMax, Y, cv=cv_)
+end4 = time.perf_counter()
+print("non_linear :: Acurracy is {}, {:.2f}sec".format(np.round(non_linear_score_with_min_max.mean(), 3), end4-start4))
