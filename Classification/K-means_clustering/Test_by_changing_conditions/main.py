c9aaef944ddebf947261107b900e7065449cb06f
can019 <jys01012@gmail.com>
2020-10-16 10:00:48 +0900

    moon plot

--- Classification/K-means_clustering/Test_by_changing_conditions/main.py
+++ Classification/K-means_clustering/Test_by_changing_conditions/main.py
@@ -2,14 +2,11 @@ from sklearn.datasets import make_moons
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 X, y = make_moons(n_samples=200, noise=0.05, random_state=0)
-plt.scatter(X[:,0], X[:,1], c=y,s=60,edgecolors='k')
-plt.show()
-X_varied
-, y_varied = make_blobs n_samples =200 , cluster_std =[1.0, 2.5, 0.5], random_stae =
-y_pred = KMeans(n_cluster=3, random_state=0).fit_predict(X_varied)
-"""
-plt.scatter(X[:,0], X[:,1], c=y_pred,s=60,edgecolors='k')
-kmeans - Kmeans(n_cluster=2)
+n_cluster = 10
+kmeans = KMeans(n_clusters=n_cluster)
 kmeans.fit(X)
-y_pred = kmeans.predict(X)
-"""
+y = kmeans.predict(X)
+plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1],
+            marker='^',s=100,linewidth=2,edgecolors='k')
+plt.scatter(X[:,0],X[:,1],c=y,marker='o',s=13)
+plt.show()
