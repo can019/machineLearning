21c45f70421fea8a594a3c1da8026f5950cc2396
can019 <jys01012@gmail.com>
2020-10-16 10:44:01 +0900

    std basic setting

--- Classification/K-means_clustering/make_blobs_changing_cluster/main.py
+++ Classification/K-means_clustering/make_blobs_changing_cluster/main.py
@@ -2,9 +2,9 @@ from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import numpy as np
-a = 300  # 극단값들이 줄어듬
-b = 10  # 데이터들이 정가운데 뭉치는 정도
-c = 0.1  # 가운데 놈의 뭉침 정도 높을수록 풀림.
+a = 1.0  # 극단값들이 줄어듬
+b = 2.5  # 데이터들이 정가운데 뭉치는 정도
+c = 0.5  # 가운데 놈의 뭉침 정도 높을수록 풀림.
 X_varied, y_varied =\
     make_blobs(n_samples=200,cluster_std=[a,b,c],random_state=170)
 plt.scatter(X_varied[:,0], X_varied[:,1], y_varied)
